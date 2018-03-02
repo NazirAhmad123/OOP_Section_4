3fdc6b4c6a8f19238f7dd87820e19174d9bdbbda
Nazir <nazirahmadsaidzada@gmail.com>
2018-03-02 19:26:57 +0800

    Update and rename assignment.txt to assignment.java

---
+++ class+02_sec_04/assignment.java
@@ -0,0 +1,26 @@
+assignment 1
+
+Create a method called displayHighScorePosition().
+It should return a player's name and position in the high score table
+You should display the players name with a message like
+(name) Managed to get into position (position) in the high score table.
+
+e.g: Ali managed to get into position 1th in the high score table.
+
+Create a second method called calculateHighScorePosition().
+It should be sent an argument, the players score.
+It should return an int (the player's position).
+
+the position should be based on the following:
+1th if the score > 1000
+2th if the score > 500 && score < 1000
+3th if the score > 100 && score < 500
+4th all other cases.
+
+call both methods and display the result of the scores of 1500, 900, 400 and 50.
+
+
+
+// Submit the source code to my email: oop_tuts@yahoo.com
+// Use your creativity to come up with the solution.
+// Good luck and have a great rest of the weekend ahead.
